048bee64f15fcb77c8e146d110d2cdffa9e4bd2f
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-03-25 08:06:28 +0000

    next step is to integrate chip API with the project as it is collecting the data from the newsletter signup.

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const bodyParser =require("body-parser");
 const request = require("request");
-// What should happen when user tries to go to homepage, the root route
+
 app.get("/", function(req,res){
-    res.send("Server is up and running");
-})
+    res.sendFile(__dirname + "/signup.html");
+});
 
+// Public is the file where all the static links are kept
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({entended:true}));
 
- 
-
-
+app.post("/",function(req, res){
+var firstName = req.body.fname;
+var surname = req.body.sname;
+var email = req.body.email;
+console.log(firstName,surname,email);
+});
 app.listen(3000, function(){
     console.log("server is running on port 3000");
-})
+});
