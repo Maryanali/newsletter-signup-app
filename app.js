64545d7cedcc05e39dfb5b827b6dd4dfedca952d
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-03-25 16:28:51 +0000

    Connecting to the mailchimp api

--- app.js
+++ app.js
@@ -1,22 +1,90 @@
+//Requiring mailchimp's module
+//For this we need to install the npm module @mailchimp/mailchimp_marketing. To do that we write:
+//npm install @mailchimp/mailchimp_marketing
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+//Requiring express and body parser and initializing the constant "app"
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const bodyParser =require("body-parser");
+const https = require('https');
 const request = require("request");
-
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/signup.html");
-});
-
-// Public is the file where all the static links are kept
+//Using bod-parser
+app.use(bodyParser.urlencoded({extended:true}));
+//The public folder which holds the CSS
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({entended:true}));
+//Listening on port 3000 and if it goes well then logging a message saying that the server is running
+app.listen(process.env.PORT||3000,function () {
+ console.log("Server is running at port 3000!");
+});
+//Sending the signup.html file to the browser as soon as a request is made on localhost:3000
+app.get("/", function (req, res) {
+ res.sendFile(__dirname + "/signup.html");
+});
+//Setting up MailChimp
+mailchimp.setConfig({
+//*****************************ENTER YOUR API KEY HERE******************************
+ apiKey: "9e87494701fb18b2084b8b0c4ad24947-us1",
+//*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
+ server: "us1"
+});
+//As soon as the sign in button is pressed execute this
+app.post("/", function (req,res) {
+//*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+const firstName = req.body.firstName;
+const secondName = req.body.secondName;
+const email = req.body.email;
+//*****************************ENTER YOU LIST ID HERE******************************
+const listId = "26347d83a1";
+//Creating an object with the users data
+const subscribingUser = {
+ firstName: firstName,
+ lastName: secondName,
+ email: email
+};
+//Uploading the data to the server
+async function run() {
+const response = await mailchimp.lists.addListMember(listId, {
+ email_address: subscribingUser.email,
+ status: "subscribed",
+ merge_fields: {
+ FNAME: subscribingUser.firstName,
+ LNAME: subscribingUser.lastName
+}
+});
+    const run = async () => {
+        try{
+      const response = await client.lists.addListMember(listId, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: subscribingUser.firstName,
+            LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response); // (optional) 
+    }catch(err){
+        console.log("error");
+        console.log(JSON.parse(err.response.error.text).detail)
+    }
+    }
+    run();
 
-app.post("/",function(req, res){
-var firstName = req.body.fname;
-var surname = req.body.sname;
-var email = req.body.email;
-console.log(firstName,surname,email);
+//If all goes well logging the contact's id
+ res.sendFile(__dirname + "/success.html")
+ console.log(
+`Successfully added contact as an audience member. The contact's id is ${
+ response.id
+ }.`
+);
+}
+
+
+//Running the function and catching the errors (if any)
+// ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LESSON*************************
+// So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+ run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
-app.listen(3000, function(){
-    console.log("server is running on port 3000");
-});
+
+// cb3b9058656776d85cb6cb5323472c1d-us1 api key
+
+//  unique id for audience nothing 26347d83a1
